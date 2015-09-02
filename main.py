3b4f2382ba860b5ef74bf0aca79d60c89550113e
Astyan <astyan@omnos.fr>
2015-09-02 17:33:14 +0200

    changing the behaviour of the top area on Injection Screen and other screen when the screen get smaller

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
