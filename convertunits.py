8019475d7387539a7394d9b968ba7bee76b4c5c6
Astyan <astyan@omnos.fr>
2015-09-29 11:15:03 +0200

    little modifs

--- convertunits.py
+++ convertunits.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-from firkin import UnitManager, SIFamily, Family
+from externlib.firkin import UnitManager, SIFamily, Family
 
 class BaseUnits:
     
